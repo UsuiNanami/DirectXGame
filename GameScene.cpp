7171e60adb295b37f13b0cee8fa6f85b1dee8834
UsuiNanami <nanami05272006@gmail.com>
2026-04-06 02:23:53 +0900

    修正

--- GameScene.cpp
+++ GameScene.cpp
@@ -17,32 +17,6 @@ void GameScene::Initialize() {}
 //====================================
 void GameScene::Update() {}
 
-	#ifdef _DEBUG // デバッグビルドのときだけ!!
-	// デバッグカメラの有効フラグを切り替える
-	if (Input::GetInstance()->TriggerKey(DIK_RETURN)) {
-		isDebugCameraActive_ = !isDebugCameraActive_;
-	}
-#endif
-
-	// カメラの処理
-	if (isDebugCameraActive_) {
-		// デバックカメラの更新
-		debugCamera_->Update();
-
-		camera_.matView = debugCamera_->GetCamera().matView;
-		camera_.matProjection = debugCamera_->GetCamera().matProjection;
-
-		// ビュープロジェクションの転送
-		camera_.TransferMatrix();
-	
-	} else {
-
-		camera_.UpdateMatrix();
-	
-	}
-
-}
-
 //====================================
 // 描画処理
 //====================================
